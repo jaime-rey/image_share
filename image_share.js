ff1b9d2be9dcb06505d1dccf41cc50652b9fff14
jaimereycasado <jaimereycasado@gmail.com>
2016-01-15 22:47:04 +0100

    array each

--- image_share.js
+++ image_share.js
@@ -1,5 +1,35 @@
 if (Meteor.isClient) {
-
+    let img_data = [
+        {
+            img_src : "cav.jpg",
+            img_alt : "lannisters always paid their debts"
+        },
+        {
+            img_src : "ali.jpg",
+            img_alt : "alita is hot"
+        },
+        {
+            img_src : "crim.jpg",
+            img_alt : "crimson king"
+        },
+        {
+            img_src : "dwarf.jpg",
+            img_alt : "bleroooooooooooo"
+        },
+        {
+            img_src : "goat.jpg",
+            img_alt : "puta cabra"
+        },
+        {
+            img_src : "scp.jpg",
+            img_alt : "don´t blink"
+        },
+        {
+            img_src : "smt.jpg",
+            img_alt : "yellow king"
+        },
+]
+    Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
