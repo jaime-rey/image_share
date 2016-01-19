7b8d579734cd95ef7e57e18550ab26667277ff2f
jaimereycasado <jaimereycasado@gmail.com>
2016-01-19 23:53:51 +0100

    security

---
+++ server/server_test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by jaime on 19/01/2016.
+ */
+console.log("running on the server, yeah");
+
---
+++ shared/shared_test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by jaime on 19/01/2016.
+ */
+console.log("running on the shared, oh yeah");
